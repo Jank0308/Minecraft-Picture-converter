a02b384d158c17247bef9de1e587892628cd1105
Jan Kursawe <118570892+Jank0308@users.noreply.github.com>
2024-04-08 15:03:36 +0200

    Add files via upload

---
+++ Image Try.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import numpy as np
+import turtle
+
+turtle.speed(0)
+
+
+image = Image.open("/home/jan/Documents/programmes/pixil-frame-0(2).png")
+
+
+data = np.asarray(image)
+
+
+
+for yax in range(0,data.shape[0]):
+   
+    for xax in range(0,data.shape[1]-1):
+        pixelcolor = (float(data[xax][yax][0])/256,float(data[xax][yax][1])/256,float(data[xax][yax][2]/256))
+        turtle.color(pixelcolor)
+        turtle.penup()
+        turtle.setpos(yax,data.shape[1]-xax)
+        print(xax,yax)
+        turtle.dot(1)
+        turtle.pendown()
+
+
+turtle.exitonclick()
